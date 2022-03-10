77d42e525e7d941872b8fb8f0c80c71e54c2c4de
Motokazu Ito <m.itou.sys21@morijyobi.ac.jp>
2022-03-10 10:24:32 +0900

    pra2.javaの変更をコミット

--- pra2.java
+++ pra2.java
@@ -1,5 +1,8 @@
 public class pra2.java{
 	public static void main(String[]args){
-		System.out.println("Hello World");
+		Stirng a="Hello World";
+		for(int i=0;i<=100;i++){
+			System.out.println(a);
+		}
 	}
 }
